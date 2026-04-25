1cb87f38a80499866bddd77a43a34eda8ca56d0b
Yaya <42711252+ayagaidi@users.noreply.github.com>
2026-04-25 21:30:26 +0200

    dd

--- config/apilifo.php
+++ config/apilifo.php
@@ -3,8 +3,7 @@
 return [
     'user_api_name' => 'LY001',
     'user_api_password' => '123456',
-    // 'user_api_password' => 'l!fo20@25*',
-    'api_url' => 'http://197.44.140.211:83/api/',
+        // 'user_api_password' => 'l!fo20@25*',
 
     // ... more options
 ];
